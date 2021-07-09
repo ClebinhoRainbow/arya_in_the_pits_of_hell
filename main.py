a11523f118f39208501ffb1fc6c5da85ae8a45cf
Cledson da Silva Araújo <ciebinho.embetant@gmail.com>
2021-07-09 17:15:24 -0300

    chore: add main

---
+++ main.py
@@ -0,0 +1,48 @@
+from PPlay.window import *
+from PPlay.gameimage import*
+from PPlay.sprite import*
+#inicializacao
+
+janela = Window(800,600)
+janela.set_title("Arya in the pits of hell")
+teclado = Window.get_keyboard()
+
+
+# fundo = GameImage('vesuvio.jpg')
+# bola = Sprite('bola de fogo.png',1)
+# padE = Sprite ("pad.png",1)
+# padD = Sprite ("pad.png",1)
+
+
+# posicao_x_bola = janela.width/2 -bola.width/2
+# posicao_y_bola = janela.height/2 - bola.height/2
+# bola.set_position(posicao_x_bola,posicao_y_bola)
+
+# pontuacaoE = 0
+# pontuacaoD = 0
+
+# padE.x = 5
+# padE.y = janela.height/2 - padE.height/2
+# padD.x = janela.width - padD.width - 5
+# padD.y = janela.height/2 - padD.height/2
+
+# velPad = 200
+# velocidadeX = 200
+# velocidadeY = 200
+#Game Loop
+while (True):
+    #entrad de dados
+
+    # Update dos Game Objects
+
+    # janela.draw_text("OI", 200, 200, size=32, color=(255, 255, 255))
+  
+    #Games Physics
+    
+
+    #Placar
+
+    #Desenho dos Game Objects
+ 
+   
+    janela.update()

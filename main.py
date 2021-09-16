e5ab4b4b44f52be3fa1a90f0675e544752d0057d
ClebinhoRainbow <cledson_silva@id.uff.br>
2021-09-16 18:46:57 -0300

    chore: fixingPlataforms

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def game():
 
         #Games Physics
 
-        scroll = player.move_player(teclado, janela,plataforma)
+        scroll = player.move_player(teclado, janela,lista_de_obstaculos)
 
         # inimigo1.move_inimigo(player.x,400, janela,plataforma)
         # inimigo2.move_inimigo(player.x,700,janela,plataforma)
@@ -93,3 +93,101 @@ def game():
         plataforma4.draw()
         print(mouse.get_position())
         janela.update()
+
+
+
+# from world import *
+# from PPlay.window import *
+# from PPlay.gameimage import *
+# from PPlay.sprite import *
+# from player import *
+# from plataforma import *
+# from inimigo import *
+# from PPlay.sound import *
+# import csv
+
+# #inicializacao
+
+# janela = Window(800, 600)
+# janela.set_title("Arya in the pits of hell")
+# teclado = Window.get_keyboard()
+# musica = Sound("assets/audio/hellOST.ogg")
+# #efx = Sound()
+# clock = pygame.time.Clock()
+# FPS = 60
+# mouse = Window.get_mouse()
+# #musica.play()
+
+# # enemy_1 = Sprite("inimigo.png",1)
+
+
+# # player = Player("arya.png", 50, 401)
+# world_data = []
+# for row in range(20):
+#     r = [0] * 60
+#     world_data.append(r)
+
+# with open("assets/fase.csv", newline='') as csvfile:
+#     reader = csv.reader(csvfile, delimiter=',')
+#     for x, row in enumerate(reader):
+#         for y, tile in enumerate(row):
+#             world_data[x][y] = int(tile)
+
+# world = World()
+# world.processa_dado(world_data)
+
+# player = Player("assets/sprite.png",6)
+# player.set_sequence_time(0,2,400)
+# inimigo1 = Inimigo("assets/flip-enemy.png")
+# inimigo2 = Inimigo("assets/flip-enemy.png")
+# player.set_position(80,401)
+# inimigo1.set_position(400, 401)
+# inimigo2.set_position(700,251)
+# plataforma = Plataforma("assets/plataforma.png", 45, 480, 1)
+# plataforma2 = Plataforma("assets/plataforma.png", 245, 480, 1)
+# plataforma3 = Plataforma("assets/plataforma.png", 445, 480, 1)
+# plataforma4 = Plataforma("assets/plataforma.png", 600, inimigo2.y + inimigo2.height, 1)
+# lista_de_inimigos = []
+# lista_plataformas = []
+
+# lista_de_obstaculos = world.get_lista_de_obstaculos()
+# lista_plataformas.append(plataforma)
+# lista_plataformas.append(plataforma2)
+# lista_plataformas.append(plataforma3)
+# lista_plataformas.append(plataforma4)
+# lista_de_inimigos.append(inimigo1)
+# lista_de_inimigos.append(inimigo2)
+
+# def game():
+#     musica.play()
+#     while (True):
+#         clock.tick(FPS)
+#         # Update dos Game Objects
+
+#         #Games Physics
+
+#         scroll = player.move_player(teclado, janela,lista_de_obstaculos)
+
+#         # inimigo1.move_inimigo(player.x,400, janela,plataforma)
+#         # inimigo2.move_inimigo(player.x,700,janela,plataforma)
+
+#         #Desenho dos Game Objects
+#         janela.set_background_color([43, 16, 41])
+#         world.desenha(janela, scroll)
+#         player.show_hud(janela.screen)
+#         player.colisao_com_plataforma(lista_de_obstaculos)
+#         for inimigo in lista_de_inimigos:
+#             inimigo.draw()
+#             inimigo.shoot(janela, player)
+#         player.update()
+#         player.draw()
+#         player.shoot(janela, teclado, lista_de_inimigos)
+#         janela.draw_text("Life: " + str(player.number_lifes), 580, 20, size=16, color=(255, 255, 255),
+#                             font_name="Arial",
+#                             bold=False, italic=False)
+#         plataforma.draw()
+#         plataforma2.draw()
+#         plataforma3.draw()
+#         plataforma4.draw()
+#         print(mouse.get_position())
+#         janela.update()        
--- player.py
+++ player.py
@@ -37,7 +37,7 @@ class Player(sprite.Sprite):
     def altera_stripe_de_movimento(self):
         print("oi")
 
-    def move_player(self,teclado,janela,plataforma):
+    def move_player(self,teclado,janela,plataformas):
         self.vel = 5
 
         screen_scroll = 0
@@ -46,18 +46,19 @@ class Player(sprite.Sprite):
         if (keyPressed and self.is_jumping == False):
 
             self.is_jumping = True
-            self.vel_y = -20
+            self.vel_y = -25
 
         
         self.vel_y += 30 * janela.delta_time()
-        #substituir plataforma por?
-        if self.y + self.vel_y + self.height +10 >= plataforma.y:
-
-            self.y = plataforma.y - self.height
-            self.is_jumping = False
-
-        else: 
-            self.y += self.vel_y
+        #substituir plataforma por? lancar um for sobre os ob
+        for tile in plataformas:
+            if tile[1].colliderect(self.x,self.y+self.vel_y+ self.height +10,self.width,self.height):
+                self.y = tile[1].top - self.height
+                self.is_jumping = False
+                break
+            else:
+                self.y += self.vel_y
+                break
 
         #print(self.get_curr_frame())
         if not self.is_looping():
@@ -194,6 +195,206 @@ class Player(sprite.Sprite):
 
         self.x += self.dx
         self.y += self.dy
+
+
+
+# import time
+# import pygame
+# from PPlay import sprite
+# from tiro import *
+# from PPlay.window import *
+# from PPlay.sound import *
+# from PPlay import gameimage
+# efx = Sound("assets/audio/shoot.ogg")
+# SCROLL_THRESH = 200
+# class Player(sprite.Sprite):
+
+#     vel = 5
+#     vel_y = 0
+#     dx = vel
+#     dy = vel_y
+
+#     ammo = True
+#     muni = 10
+#     total_life = 50
+#     number_lifes = 50
+#     item = True
+#     looking_right = True
+#     is_jumping = False
+#     relogio = 0
+#     action = 0
+#     lista_tiros = []
+#     delta_1 = time.time()
+#     delta_0 = time.time()
+#     shoot_rate = 0.75
+#     falling = False
+#     def init(self, image_file):
+#         super().init(image_file, frames=1)
+#         self = pygame.transform.scale(self, (int(self.width * 3), int(self.height * 3)))
+        
+#         # self.width = self.image.get_rect().width
+#         # self.height = self.image.get_rect().height
+#     def altera_stripe_de_movimento(self):
+#         print("oi")
+
+#     def move_player(self,teclado,janela,plataformas):
+#         self.vel = 5
+
+#         screen_scroll = 0
+#         #fisica do salto
+#         keyPressed = teclado.key_pressed("UP")
+#         if (keyPressed and self.is_jumping == False):
+
+#             self.is_jumping = True
+#             self.vel_y = -20
+
+        
+#         self.vel_y += 30 * janela.delta_time()
+#         #substituir plataforma por? lancar um for sobre os ob
+#         for tile in plataformas:
+#             if tile[1].colliderect(self.x,self.y+self.vel_y+ self.height +10,self.width,self.height):
+#                 self.y = tile[1].top - self.height
+#                 self.is_jumping = False  
+#                 break
+#             else: 
+#                 self.y += self.vel_y
+#                 break
+
+#         #print(self.get_curr_frame())
+#         if not self.is_looping():
+#             print('parou')
+#         if not self.is_playing():
+#             print('stoped')
+#         # fisica de movimento
+
+#         keyLeftPressed = teclado.key_pressed("LEFT") and self.x > 0
+#         keyRightPressed = teclado.key_pressed("RIGHT") and self.x < janela.width - self.width
+#         if (keyLeftPressed or keyRightPressed):
+
+#             if (keyLeftPressed):
+#                 self.x = self.x - self.vel
+#                 self.looking_right = False
+#                 self.flip_player()
+#             if (keyRightPressed):
+#                 self.x = self.x + self.vel
+#                 self.looking_right = True
+#                 self.flip_player()
+#             self.upd_action(1)
+#         else:
+#             self.upd_action(0)
+
+#         if self.x + self.width > 800 - SCROLL_THRESH:
+#             self.x -= self.vel
+#             screen_scroll = -self.vel
+#         elif self.x < 100:
+#             self.x += self.vel
+#             screen_scroll = self.vel
+#         return screen_scroll
+          
+#     def upd_action(self, new_action):
+#         if self.action != new_action:
+#             if new_action == 1:
+#                 self.set_sequence_time(2,6,160)
+#             if new_action == 0:
+#                 print("passou")
+#                 self.set_sequence_time(0,2,400)
+#             self.action = new_action
+
+#     def flip_player(self):
+#         #TODO
+#         if(self.looking_right == True):
+#             self.image = pygame.image.load("assets/sprite.png")
+#             #self.x -= self.width/2
+#         else:
+#             self.image = pygame.image.load("assets/sprite1.png")
+#             #self.x += self.width/2
+#             pass
+
+#     def shoot(self,janela,teclado,lInimigos):
+#         lista_tiro = []
+#         cooldown = 0
+#         altura_arma = self.y + 15
+#         if self.looking_right:
+#             distancia_inicial_tiro = self.x + 50
+#         else:
+#             distancia_inicial_tiro = self.x
+
+#         self.delta_1 = time.time()
+#         cooldown = self.delta_1 - self.delta_0
+#         if (teclado.key_pressed("SPACE")   and cooldown > self.shoot_rate ):
+#             efx.play()
+#             self.muni -= 1
+#             self.delta_0 = time.time()
+#             #Instancia tiro
+#             novo_tiro = Tiro("assets/tiro.png")
+#             if self.looking_right:
+#                 novo_tiro.dire = 1
+#             else:
+#                 novo_tiro.dire = -1
+#             novo_tiro.set_position(distancia_inicial_tiro, altura_arma)
+            
+#             self.lista_tiros.append(novo_tiro)
+
+
+
+#         for tiro in self.lista_tiros:
+#             tiro.x = tiro.x + (tiro.vel * tiro.dire)
+#             for x in lInimigos:
+#                 if tiro.collided(x):
+#                     self.lista_tiros.pop(0)
+#                     lInimigos.remove(x)
+#             if (tiro.x > janela.width or tiro.x < 0):
+#                 self.lista_tiros.pop(0)
+#                 continue
+#             #tiro.set_position(tiro.x+5,tiro.y)
+#             tiro.draw()
+
+
+#     def show_hud(self, screen):
+#         hud = GameImage("assets/Hudd.png")
+#         hud.set_position(25, 25)
+#         hud.draw()
+#         ratio = self.number_lifes / self.total_life
+#         pygame.draw.rect(screen, (0,255,0), (29, 33, 115*ratio, 8))
+#         if(self.item):
+#             #drawItem
+#             item = GameImage("assets/item.png")
+#             item.set_position(178,33)
+#             item.draw()
+#         if(self.ammo):
+#             #subtract the number of bullets
+#             for x in range(self.muni):
+#                 bullet = GameImage("assets/ammo.png")
+#                 bullet.set_position(29 +( bullet.width * (x*1.5)) , 50 )
+#                 bullet.draw()
+        
+#     def decrease_life(self):
+#         self.number_lifes -= 1
+
+#     def colidiu(self, list_of_monsters, list_of_enemy_fire):
+#         for monster in list_of_monsters:
+#             if(self.colidiu(monster)):
+#                 self.decrease_life()
+
+#     def colisao_com_plataforma(self,lista_de_obstaculos):
+#         self.dy = 0
+#         self.dx = 0
+#         for tile in lista_de_obstaculos:
+#             if tile[1].colliderect(self.x+self.vel,self.y,self.width,self.height):
+#                 self.dx = 0
+#             #     self.vel_y = 0
+#             if tile[1].colliderect(self.x,self.y+self.vel_y,self.width,self.height):
+#                 if self.vel_y < 0:
+#                     self.vel_y = 0
+#                     self.dy = tile[1].bottom - self.y
+#                     # self.is_jumping = True
+#                 elif self.vel_y >= 0:
+#                     self.vel_y = 0
+#                     self.is_jumping = False
+#                     self.dy = tile[1].top - (self.y+self.height)
+
+#         self.x += self.dx
+#         self.y += self.dy
 
 
 
